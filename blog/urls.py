23c88d1db1b46357fe1f03e3a7601e9555db775a
wuze <ze.wu@campus.tu-berlin.de>
2026-05-24 14:53:14 +0200

    init django blog

--- blog/urls.py
+++ blog/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('post/<slug:slug>/', views.post_detail, name='post_detail'),
-    path('category/<slug:slug>/', views.category_posts, name='category_posts'),
+    path('post/<str:slug>/', views.post_detail, name='post_detail'),
+    path('category/<str:slug>/', views.category_posts, name='category_posts'),
     path('search/', views.search, name='search'),
     path('login/', views.login_register_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
